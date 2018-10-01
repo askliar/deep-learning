72492b2d6f4dbc870845dbe3431871de525d7767
emohat1r <andreyws96@gmail.com>
2018-10-01 18:28:45 +0200

    fix job

--- assignment_2/jobs/part3_job_script.sh
+++ assignment_2/jobs/part3_job_script.sh
@@ -8,7 +8,6 @@ cd $HOME/uvadlc_practicals_2018/assignment_2/part3
 
 echo "Job $PBS_JOBID started at `date`; text generation job is started" | mail $USER -s "Job $PBS_JOBID"
 
-python train.py --txt_file books/book_of_jungle.txt --device 'cuda:0' --temperature "$TEMPERATURE" --sampling "$SAMPLING"
-python train.py --txt_file books/Brodskiy.txt --device 'cuda:1' --temperature "$TEMPERATURE" --sampling "$SAMPLING"
+python train.py --txt_file books/vanity_fair.txt --device 'cuda:0' --temperature "$TEMPERATURE" --sampling "$SAMPLING"
 
 echo "Job $PBS_JOBID ended at `date`; text generation job is finished" | mail $USER -s "Job $PBS_JOBID"
